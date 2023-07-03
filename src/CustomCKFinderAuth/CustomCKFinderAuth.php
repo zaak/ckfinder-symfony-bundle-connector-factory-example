ddeaa733fd15b04360c00f4025c7c0861f2b30cc
Paweł Wiaderny <zaak@users.noreply.github.com>
2023-07-03 09:41:12 +0200

    Add CKinder auth

---
+++ src/CustomCKFinderAuth/CustomCKFinderAuth.php
@@ -0,0 +1,15 @@
+<?php
+
+// src/CustomCKFinderAuth/CustomCKFinderAuth.php
+
+namespace App\CustomCKFinderAuth;
+
+use CKSource\Bundle\CKFinderBundle\Authentication\Authentication as AuthenticationBase;
+
+class CustomCKFinderAuth extends AuthenticationBase
+{
+    public function authenticate(): bool
+    {
+        return true;
+    }
+}
